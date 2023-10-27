9ffe6c18b2610fefc65029b79febbb72ddb869b7
avivbh1 <avivben2004@gmail.com>
2023-10-27 18:25:33 +0300

    Based simple api for controlling the tracking sniffing

--- services.py
+++ services.py
@@ -1,22 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
-from main import change_track_state, get_track_state
+from states import get_tracking_state, change_tracking_state
 app = FastAPI()
 
 
 @app.put("/track/state/{state}")
 def handle_change_track_state(state: bool):
-    change_track_state(state)
+    change_tracking_state(state)
 
 
 @app.get("/track/state")
 def handle_get_track_state():
-    return get_track_state()
-
-
-@app.get("/")
-def hello(name: str):
-    return f"Hello, This is a plain text response."
+    return get_tracking_state()
 
 
 uvicorn.run(app, host="0.0.0.0", port=8000)
